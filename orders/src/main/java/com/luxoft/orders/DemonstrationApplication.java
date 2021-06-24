fd2e565eb2cadb09dc623c3ade6e7932ee51e49e
Nikolai Osipov <nao99.dev@gmail.com>
2021-06-24 15:09:16 +0700

    chore(*): add class for functionality demonstration

---
+++ orders/src/main/java/com/luxoft/orders/DemonstrationApplication.java
@@ -0,0 +1,75 @@
+package com.luxoft.orders;
+
+import com.luxoft.orders.api.CreateOrderDto;
+import com.luxoft.orders.api.CreateOrderItemDto;
+import com.luxoft.orders.domain.JdbcOrderItemRepository;
+import com.luxoft.orders.domain.JdbcOrderRepository;
+import com.luxoft.orders.domain.OrderServiceImpl;
+import com.luxoft.orders.persistent.query.JdbcTemplateImpl;
+import com.luxoft.orders.persistent.transaction.JdbcTransactionRunner;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * DemonstrationApplication class
+ *
+ * @author  Nikolai Osipov <nao99.dev@gmail.com>
+ * @version 1.0.0
+ * @since   2021-06-24
+ */
+public class DemonstrationApplication {
+    public static void main(String[] args) {
+        // 0. Preparation
+        HikariDataSource dataSource = new HikariDataSource();
+
+        dataSource.setAutoCommit(false);
+        dataSource.setUsername("dbuser");
+        dataSource.setPassword("dbuser");
+        dataSource.setJdbcUrl("jdbc:postgresql://172.50.1.2/app");
+
+        var jdbcTemplate = new JdbcTemplateImpl();
+        var jdbcTransactionRunner = new JdbcTransactionRunner(dataSource);
+
+        var jdbcOrderItemRepository = new JdbcOrderItemRepository(jdbcTemplate);
+        var jdbcOrderRepository = new JdbcOrderRepository(jdbcTemplate, jdbcOrderItemRepository);
+
+        var orderService = new OrderServiceImpl(jdbcTransactionRunner, jdbcOrderRepository, jdbcOrderItemRepository);
+
+        // 1. Create order
+        var createOrderItemDtoShoes = new CreateOrderItemDto();
+
+        createOrderItemDtoShoes.setName("Shoes");
+        createOrderItemDtoShoes.setCount(15);
+        createOrderItemDtoShoes.setPrice(BigDecimal.valueOf(1500L));
+
+        var createOrderDto = new CreateOrderDto();
+
+        createOrderDto.setUsername("Alex");
+        createOrderDto.setItems(List.of(createOrderItemDtoShoes));
+
+        var createdOrder = orderService.createOrder(createOrderDto);
+
+        // 2.Get order
+        var selectedOrder = orderService.getOrder(createdOrder.getId());
+
+        // 2.1 Print order
+        System.out.println(selectedOrder);
+
+        // 3. Add order item
+        var createOrderItemDtoGloves = new CreateOrderItemDto();
+
+        createOrderItemDtoGloves.setName("Gloves");
+        createOrderItemDtoGloves.setCount(30);
+        createOrderItemDtoGloves.setPrice(BigDecimal.valueOf(100L));
+
+        orderService.addOrderItem(selectedOrder.getId(), createOrderItemDtoGloves);
+
+        // 4.Change order item count
+        orderService.changeOrderItemCount(selectedOrder.getItems().get(0).getId(), 10);
+
+        // 5. Done all orders
+        orderService.doneAllOrders();
+    }
+}
